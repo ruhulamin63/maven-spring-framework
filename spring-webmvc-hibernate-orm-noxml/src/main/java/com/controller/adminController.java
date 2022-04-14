577f30cd4d0aab0405ff081637e0419d851f59b6
ruhulamin63 <raridoy4@gmail.com>
2022-04-14 20:41:36 +0600

    source code upload

---
+++ spring-webmvc-hibernate-orm-noxml/src/main/java/com/controller/adminController.java
@@ -0,0 +1,15 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+
+public class adminController {
+
+    @RequestMapping("/test")
+    public String index() {
+        return "adminDashboard";
+    }
+}
